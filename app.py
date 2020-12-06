e195f0c3c75673224cf4ae6c3312b2668ee25815
Emre Savaş <e@emresavas.com>
2020-12-06 21:11:54 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Hello(Resource):
+    def get(self):
+        return {'status': 'success', 'message': 'Hello! This is your first API request.'}
+
+api.add_resource(Hello, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
